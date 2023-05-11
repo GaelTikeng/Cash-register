404d5df81cc6d9a89f39dfe0d4f8bed67a28fcce
GaelTikeng <Gaelinho@yahoo.fr>
2023-05-11 14:00:45 +0100

    change display

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let currencyNotesDisplay = document.querySelector('.display')
 // let cid = document.querySelector('.cash_register_state')
 
-// let cid = [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
+let cid = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
 
 let object1 = {
   status: "INSUFFICIENT_FUND",
@@ -20,34 +20,35 @@ let object3 = {
 }
 
 function cashRegister() {
-  let cid[0][1] = +document.querySelector('#penny').value,
-      cid[1][1] = +document.querySelector('#nickel').value,
-      cid[2][1] = +document.querySelector('#dime').value,
-      cid[3][1] = +document.querySelector('#quarter').value,
-      cid[4][1] = +document.querySelector('#one').value,
-      cid[5][1] = +document.querySelector('#five').value,
-      cid[6][1] = +document.querySelector('#ten').value,
-      cid[7][1] = +document.querySelector('#twenty').value,
+  let a = +document.querySelector('#penny').value,
+      b = +document.querySelector('#nickel').value,
+      c = +document.querySelector('#dime').value,
+      d = +document.querySelector('#quarter').value,
+      e = +document.querySelector('#one').value,
+      f = +document.querySelector('#five').value,
+      g = +document.querySelector('#ten').value,
+      h = +document.querySelector('#twenty').value,
       B = +document.querySelector('.bill').value,
       N = +document.querySelector('.payment').value,
       balance = N - B,
       wholeBalance = Math.floor(balance),
       decimalBalance = balance - Math.floor(balance),
+      arr = [a, b, c, d, e, f, g, h]
       whole = 0,
       modolus = 0,
       sum = 0,
 			x = 0,
 			y = 0
   // let CID =cid
-  
+  console.log(arr[1])
   console.log(typeof N)
   console.log(N)
   console.log(typeof B)
   console.log(B)
   console.log(typeof balance)
   console.log(balance)
-	for (let i = 0; i < cid.length; i++) {
-		sum = +(sum + cid[i][1]).toFixed(2)
+	for (let i = 0; i < arr.length; i++) {
+		sum = +(sum + arr[i]).toFixed(2)
 	}
 	if (sum < balance) {
 		document.querySelector('.display').innerHTML = JSON.stringify(object1)
@@ -57,36 +58,37 @@ function cashRegister() {
     document.querySelector('.display').innerHTML = JSON.stringify(object3)
     // console.log(object3)
 	} else {
-    if (cid[7][1] !== 0) {
+    if (arr[7] !== 0) {
       whole = Math.floor(wholeBalance/20)
       modolus = wholeBalance % 20
       x = whole * 20
       // test if the number of twenty notes we have to give is greater than what is available in the cid
-      if (x >= cid[7][1]) {
-        y = x - cid[7][1]
-        cid[7][1] = x - y
+      if (x >= arr[7]) {
+        y = x - arr[7]
+        arr[7] = x - y
       } else {
-        y = cid[7][1] - x
-        cid[7][1] -= y
+        y = arr[7] - x
+        arr[7] -= y
       }
     }
-    if (cid[7][1] !== 0) {
-      object2.change.push(cid[7])
-    }
-    if (cid[6][1] !== 0 && cid[7][1] === 0) {
+
+    // 
+    cid[7][1] = arr[7]
+    object2.change.push(cid[7])
+    if (arr[6] !== 0 && arr[7] === 0) {
       whole = Math.floor(wholeBalance/10)
       modolus = wholeBalance % 10
       x = whole * 10
-      if (x >= cid[6][1]) {
-        y = x - cid[6][1]
-        cid[6][1] = x - y
+      if (x >= arr[6]) {
+        y = x - arr[6]
+        arr[6] = x - y
       } else {
-        y = cid[6][1] - x
-        cid[6][1] -= y
+        y = arr[6] - x
+        arr[6] -= y
       }
-    } else if (cid[6][1] !== 0) {
+    } else if (arr[6] !== 0) {
       // in case the number of twenty in the cid < what is needed, we handle the difference
-      if(x > cid[7][1]) {
+      if(x > arr[7]) {
         whole = Math.floor((y + modolus)/10)
       } else {
         whole = Math.floor(modolus/10)
@@ -94,233 +96,224 @@ function cashRegister() {
       modolus = modolus % 10
       x = whole * 10
       // test if the amount needed is greater that the available
-      if (x >= cid[6][1]) {
-        y = x - cid[6][1]
-        cid[6][1] = x - y
+      if (x >= arr[6]) {
+        y = x - arr[6]
+        arr[6] = x - y
       } else {
-        y = cid[6][1] - x
-        cid[6][1] -= y
+        y = arr[6] - x
+        arr[6] -= y
       }
     } 
-    if (cid[6][1] !== 0) {
-      console.log(cid[6])
-      console.log(cid[6][1])
-      console.log(typeof cid[6][1])
-      object2.change.push(cid[6])
-    }
+    cid[6][1] = arr[6]
+    object2.change.push(cid[6])
   
     // test if value of key "five" and "ten" are not 0 otherwise we push directly to "cahnge" 
-    if (cid[5][1] !== 0) {
-      if (x > cid[6][1]) {
+    if (arr[5] !== 0) {
+      if (x > arr[6]) {
         whole = Math.floor((modolus + y)/5)
       } else {
         whole = Math.floor(modolus/5)
       }
       modolus = modolus % 5
       x = whole * 5
-      if (x >= cid[5][1]) {
-        y = x - cid[5][1]
-        cid[5][1] = x - y
+      if (x >= arr[5]) {
+        y = x - arr[5]
+        arr[5] = x - y
       } else {
-        y = cid[5][1] - x
-        cid[5][1] -= y
+        y = arr[5] - x
+        arr[5] -= y
       }
-    } else if (cid[5][1] !== 0 && cid[6][1] === 0) {
+    } else if (arr[5] !== 0 && arr[6] === 0) {
       whole = Math.floor(wholeBalance/5)
       modolus = wholeBalance % 5
       x = whole * 5
   
-      if (x >= cid[5][1]) {
-        y = x - cid[5][1]
-        cid[5][1] = x - y
+      if (x >= arr[5]) {
+        y = x - arr[5]
+        arr[5] = x - y
       } else {
-        y = cid[5][1] - x
-        cid[5][1] -= y
+        y = arr[5] - x
+        arr[5] -= y
       }
     }
-    if (cid[5][1] !== 0) {
-      object2.change.push(cid[5])
-    }
+    cid[5][1] = arr[5]
+    object2.change.push(cid[5])
   
     // test if value of key "one" is not 0 otherwise we push directly to "cahnge" 
-    if (cid[4][1] !== 0) {
+    if (arr[4] !== 0) {
       // test this to ensure that the program clearly remove what is needed
-      if(x > cid[5][1]) {
+      if(x > arr[5]) {
         x = y + modolus
       } else {
         x = modolus
       }
-      if (x >= cid[4][1]) {
-        y = x - cid[4][1]
-        cid[4][1] = x - y
+      if (x >= arr[4]) {
+        y = x - arr[4]
+        arr[4] = x - y
       } else {
-        y = cid[4][1] - x
-        cid[4][1] -= y
+        y = arr[4] - x
+        arr[4] -= y
       }
-    } else if (cid[4][1] !== 0 && cid[5][1] === 0) {
+    } else if (arr[4] !== 0 && arr[5] === 0) {
       whole = Math.floor(wholeBalance/1)
       modolus = wholeBalance % 1
       x = whole * 1
-      if (x >= cid[4][1]) {
-        y = x - cid[4][1]
-        cid[4][1] = x - y
+      if (x >= arr[4]) {
+        y = x - arr[4]
+        arr[4] = x - y
       } else {
-        y = cid[4][1] - x
-        cid[4][1] -= y
+        y = arr[4] - x
+        arr[4] -= y
       }
     }
-    if (cid[4][1] !== 0) {
-      object2.change.push(cid[4])
-    }
+    cid[4][1] = arr[4]
+    object2.change.push(cid[4])
   
     // handeling the decimal part of the diffence btw N & B (balance)
     let dB = decimalBalance * 100, // multiply by 100 to take calculate the modolus and ease calculations
         dBwhole = 0,
         dBmodolus = 0
-    if (cid[3][1] !== 0) {
+    if (arr[3] !== 0) {
       dBwhole = Math.floor(dB/25)
       dBmodolus = dB  % 25
       x = (dBwhole * 25)/100 // divide by 100 to come back to the initial value
       // test if the number of twenty notes we have to give is greater than what is available in the cid
-      if (x >= cid[3][1]) {
-        y = x - cid[3][1]
-        cid[3][1] = x - y
+      if (x >= arr[3]) {
+        y = x - arr[3]
+        arr[3] = x - y
       } else {
-        y = cid[3][1] - x
-        cid[3][1] -= y
+        y = arr[3] - x
+        arr[3] -= y
       }
     }
-    if (cid[3][1] !== 0) {
-      object2.change.push(cid[3])
-    }
-    if (cid[2][1] !== 0) {
-      if(x > cid[3][1]) {
+    cid[3][1] = arr[3]
+    object2.change.push(cid[3])
+    if (arr[2] !== 0) {
+      if(x > arr[3]) {
         dBwhole = Math.floor((y + dBmodolus)/10)
       } else {
         dBwhole = Math.floor(dBmodolus/10)
       }
       dBmodolus = dBmodolus % 10
       x = (dBwhole * 10)/100
-      if (x >= cid[2][1]) {
-        y = x - cid[2][1]
-        cid[2][1] = +(x - y).toFixed(2)
+      if (x >= arr[2]) {
+        y = x - arr[2]
+        arr[2] = +(x - y).toFixed(2)
       } else {
-        y = cid[2][1] - x
-        cid[2][1] = +(cid[2][1] - y).toFixed(2)
+        y = arr[2] - x
+        arr[2] = +(arr[2] - y).toFixed(2)
       }
-    } else if (cid[2][1] !== 0 && cid[3][1] === 0) {
+    } else if (arr[2] !== 0 && arr[3] === 0) {
       dBwhole = Math.floor(dB/10)
       dBmodolus = dB % 10
       x = (dBwhole * 10)/100
-      if (x >= cid[2][1]) {
-        y = x - cid[2][1]
-        cid[2][1] = +(x - y).toFixed(2)
+      if (x >= arr[2]) {
+        y = x - arr[2]
+        arr[2] = +(x - y).toFixed(2)
       } else {
-        y = cid[2][1] - x
-        cid[2][1] = +(cid[2][1] - y).toFixed(2)
+        y = arr[2] - x
+        arr[2] = +(arr[2] - y).toFixed(2)
       }
     }
-    if (cid[2][1] !== 0) {
-      object2.change.push(cid[2])
-    }
-    if (cid[1][1] !== 0) {
-      if(x > cid[2][1]) {
+    cid[2][1] = arr[2]
+    object2.change.push(cid[2])
+
+    if (arr[1] !== 0) {
+      if(x > arr[2]) {
         dBwhole = Math.floor((y + dBmodolus)/5)
       } else {
         dBwhole = Math.floor(dBmodolus/5)
       }
       dBmodolus = dBmodolus % 5
       x = (dBwhole * 5)/100
-      if (x >= cid[1][1]) {
-        y = x - cid[1][1]
-        cid[1][1] = +(x - y).toFixed(2)
+      if (x >= arr[1]) {
+        y = x - arr[1]
+        arr[1] = +(x - y).toFixed(2)
       } else {
-        y = cid[1][1] - x
-        cid[1][1] = +(cid[1][1] - y).toFixed(2)
+        y = arr[1] - x
+        arr[1] = +(arr[1] - y).toFixed(2)
       }
-    } else if (cid[1][1] !== 0 && cid[2][1] === 0) {
+    } else if (arr[1] !== 0 && arr[2] === 0) {
       dBwhole = Math.floor(dB/5)
       dBmodolus = dB % 10
       x = (dBwhole * 5)/100
-      if (x >= cid[1][1]) {
-        y = x - cid[1][1]
-        cid[1][1] = +(x - y).toFixed(2)
+      if (x >= arr[1]) {
+        y = x - arr[1]
+        arr[1] = +(x - y).toFixed(2)
       } else {
-        y = cid[1][1] - x
-        cid[1][1] = +(cid[1][1] - y).toFixed(2)
+        y = arr[1] - x
+        arr[1] = +(arr[1][1] - y).toFixed(2)
       }
     }
-    if (cid[1][1] !== 0) {
-      object2.change.push(cid[1])
-    }
-    if (cid[0][1] !== 0) {
-      if(x > cid[1][1]) {
+    cid[1][1] = arr[1]
+    object2.change.push(cid[1])
+    if (arr[0] !== 0) {
+      if(x > arr[1]) {
         dBwhole = Math.floor((y + dBmodolus))
       } else {
         dBwhole = Math.floor(dBmodolus)
       }
       dBmodolus = dBmodolus % 1
       x = (dBwhole * 1)/100
-      if (x >= cid[0][1]) {
-        y = x - cid[0][1]
-        cid[0][1] = +(x - y).toFixed(2)
+      if (x >= arr[0]) {
+        y = x - arr[0]
+        arr[0] = +(x - y).toFixed(2)
       } else {
-        y = cid[0][1] - x
-        cid[0][1] = +(cid[0][1] - y).toFixed(2)
+        y = arr[0] - x
+        arr[0] = +(arr[0] - y).toFixed(2)
       }
-    } else if (cid[0][1] !== 0 && cid[1][1] === 0) {
+    } else if (arr[0] !== 0 && arr[1] === 0) {
       dBwhole = Math.floor(dB/1)
       dBmodolus = dB % 1
       x = (dBwhole * 1)/100
-      if (x >= cid[0][1]) {
-        y = x - cid[0][1]
-        cid[0][1] = +(x - y).toFixed(2)
+      if (x >= arr[0]) {
+        y = x - arr[0]
+        arr[0] = +(x - y).toFixed(2)
       } else {
-        y = cid[0][1] - x
-        cid[0][1] = +(cid[0][1] - y).toFixed(2)
+        y = arr[0] - x
+        arr[0] = +(arr[0] - y).toFixed(2)
       }
     }
-    if (cid[0][1] !== 0) {
-      object2.change.push(cid[0])
-    }
-    document.querySelector('.display-currency-notes').innerHTML = `
-    <h4> Your change is </h4>
-    <div class="cid">
-      <label for="penny">PENNY</label>
-      <input type="number" value="${cid[0][1]}" class="input">
+    cid[0][1] = arr[0]
+    object2.change.push(cid[0])
+   
+    document.querySelector('.display-text').innerHTML = `
+		<h3>Your change</h3>
+    <div class="display-currency-notes">
+      <div class="cid">
+        <label for="penny">PENNY</label>
+        <input type="number" value="${cid[0][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="nickel">NICKEL</label>
+        <input type="number" value="${cid[1][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="dime">DIME</label>
+        <input type="number" value="${cid[2][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="quarter">QUARTE</label>
+        <input type="number" value="${cid[3][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="one">ONE</label>
+        <input type="number" value="${cid[4][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="five">FIVE</label>
+        <input type="number" value="${cid[5][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="ten">TEN</label>
+        <input type="number" value="${cid[6][1]}" class="input">
+      </div>
+      <div class="cid">
+        <label for="twenty">TWENTY</label>
+        <input type="number" value="${cid[7][1]}" class="input">
+      </div>
     </div>
-    <div class="cid">
-      <label for="nickel">NICKEL</label>
-      <input type="number" value="${cid[1][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="dime">DIME</label>
-      <input type="number" value="${cid[2][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="quarter">QUARTE</label>
-      <input type="number" value="${cid[3][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="one">ONE</label>
-      <input type="number" value="${cid[4][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="five">FIVE</label>
-      <input type="number" value="${cid[5][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="ten">TEN</label>
-      <input type="number" value="${cid[6][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="twenty">TWENTY</label>
-      <input type="number" value="${cid[7][1]}" class="input">
-    </div>
-    <div class="cid">
-      <label for="onehundred">ONE HUNDRED</label>
-      <input type="number" value="${cid[8][1]}" class="input">
-    </div>`
+    `
     console.log(cid[1])
     console.log(cid[2])
     console.log(cid[3])
